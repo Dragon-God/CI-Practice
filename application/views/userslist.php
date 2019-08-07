dc604f3b031b5373807ef141a8e6affebb6dfe22
Alafin Oluwatobi <tobi.alafin@gmail.com>
2019-08-07 10:58:22 +0100

    Initial Commit

---
+++ application/views/userslist.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+
+<body>
+	<form method="get" action="">
+		<div class="form-group">
+			<label for="standardID">StandardID</label>
+			<select id="standardID" class="custom-select" name="standardID">
+				<option value="">select</option>
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="gender">Gender</label>
+			<select id="gender" class="custom-select" name="gender">
+				<option value="">select</option>
+			</select>
+		</div>
+	</form>
+</body>
+
+</html>
